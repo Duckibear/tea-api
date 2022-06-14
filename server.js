0e6b0e5f48c013cab6ace8238e61adb34e9fc3f0
Erica S <97710799+Duckibear@users.noreply.github.com>
2022-06-14 16:56:41 -0700

    Upload .gitignore, index.html, server.js

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const tea = {
+     'oolong': {
+          'type': 'black',
+          'origin': 'Yogi',
+          'waterTemp': 200,
+          'steepTimeSeconds': 180,
+          'caffine': true,
+          'flavor': 'Vanilla Spice'
+     }, 
+     'matcha': {
+          'type': 'green',
+          'origin': 'Yogi',
+          'waterTemp': 195,
+          'steepTimeSeconds': 150,
+          'caffine': false,
+          'flavor': 'Bitter'
+     }, 
+     'white': {
+          'type': 'white',
+          'origin': 'Yogi',
+          'waterTemp': 195,
+          'steepTimeSeconds': 180,
+          'caffine': false,
+          'flavor': 'Delicious'
+     },
+     'unknown': {
+          'type': 'unknown',
+          'origin': 'Yogi',
+          'waterTemp': 195,
+          'steepTimeSeconds': 180,
+          'caffine': false,
+          'flavor': 'mysterious'
+     }
+}
+
+app.get('/', (request, response) => {
+     response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (request, response) => {
+     const teaName = request.params.name.toLowerCase()
+     if ( tea[teaName] ) {
+          response.json(tea[teaName])
+     } else {
+          response.json(tea['unknown'])
+     }
+})
+
+app.listen(process.env.PORT || PORT, () => {
+     console.log('The server is running on port ${PORT}! Betta Go Catch it~')
+})
